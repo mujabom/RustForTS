33422b2805cd2a8ad470a336c954bba923aa4da9
mujabom <msaabwsr@gmail.com>
2024-06-16 20:27:13 +0300

    added into_iter case

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,9 @@ fn main() {
         collected.push(val);
     }
     println!("yay it works{:?}",collected);
+
+    //my sol 2 using into_iter
+    // let res:Vec<i32> = vec![1,2,3].into_iter().map(|x| x+1).collect();
+
+    println!("my directly mutated map {:?}", vec![1,2,3].into_iter().map(|x| x+1).collect::<Vec<i32>>())
 }
