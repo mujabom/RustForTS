3cc05ed2fb4212cc4969450969b8cc34a8d561cb
mujabom <msaabwsr@gmail.com>
2024-06-17 02:10:41 +0300

    Enum magic

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
-use std::collections::HashMap;
-
-fn main() {
-    //my solution
-    let mut c = vec![1, 2, 3];
-    c = c.iter().map(|x| x + 1).collect();
-    println!("{:?}", c);
-
-    //copied from course sol:1
-    let foo: Vec<usize> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
-    println!("{:?}", foo);
-
-    //from course sol:2
-    let data = vec![1, 2, 3];
-    let mut iter = data.iter().map(|x| x + 1);
-    let mut collected = vec![];
-    while let Some(val) = iter.next() {
-        collected.push(val);
-    }
-    println!("yay it works{:?}", collected);
-
-    //my sol 2 using into_iter
-    // let res:Vec<i32> = vec![1,2,3].into_iter().map(|x| x+1).collect();
-
-    println!(
-        "my directly mutated map {:?}",
-        vec![1, 2, 3]
-            .into_iter()
-            .map(|x| x + 1)
-            .collect::<Vec<i32>>()
-    );
-    test_hash_map()
+struct Custom {
+    age: u16,
+    name: String,
 }
 
-fn test_hash_map() {
-    let map: HashMap<usize, &str> = vec!["this", "is", "test"]
-        .into_iter()
-        .enumerate()
-        .map(|(idx, value)| (idx, value))
-        .collect();
-    println!("{:?}", map)
+enum Item {
+    Number(u16),
+    String(String),
+    MyCustom(Custom),
+}
+
+fn append(items: &mut Vec<Item>) {
+    items.push(Item::MyCustom(Custom {
+        age: 20,
+        name: "John".to_string(),
+    }));
+}
+fn main() {
+    let mut items: Vec<Item> = vec![];
+    append(&mut items);
+    //print first item
+    match items.get(0) {
+        Some(Item::String(s)) => println!("{}", s),
+        Some(Item::Number(n)) => println!("{}", n),
+        Some(Item::MyCustom(Custom { age, name })) => println!("{} {}", age, name),
+        None => println!("No item found"),
+    }
 }
