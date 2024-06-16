31e251aa398113e7d5ca47595738c85dc8d4d803
mujabom <msaabwsr@gmail.com>
2024-06-16 21:39:55 +0300

    iterating over files init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-
 fn main() {
-
+    //my solution before course
+    let file = std::fs::read_to_string("src/project/lines.txt");
+    match file {
+        Ok(content) => {
+            content.lines()
+                .enumerate()
+                .filter(|(i,_)| i % 2 == 0)
+                .for_each(|(_,line)| {
+                println!(".{}", line);
+            });
+        }
+        Err(_) => {
+            println!("Failed to read file");
+        }
+    }
 }
-
